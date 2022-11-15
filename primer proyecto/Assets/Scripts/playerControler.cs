df42f560d892708362951d64e5130e8f1d8aa5d4
soytmen <114388813+soytmen@users.noreply.github.com>
2022-11-15 12:50:24 +0100

    4

--- primer proyecto/Assets/Scripts/playerControler.cs
+++ primer proyecto/Assets/Scripts/playerControler.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class playerControler: MonoBehaviour
 {
-    public float speed = 20f;
-    public float turnSpeed = 10f;
+    private float speed = 20f;
+    private float turnSpeed = 30f;
     private float horizontalInput;
-
+    private float verticalInput;
 
 
     void Start()
@@ -21,10 +21,12 @@ public class playerControler: MonoBehaviour
 
         //detectar imputhorizontal
         horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
         // movimiento hacia adelante
         // transform.Translate(0, 0, 1); 
-        transform.Translate(Vector3.forward * 20 * Time.deltaTime);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
         //movimiento lateral manual
-        transform.Translate(Vector3.right * turnSpeed * Time.deltaTime * horizontalInput);
+        //transform.Translate(Vector3.right * turnSpeed * Time.deltaTime * horizontalInput);
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
     }
 }
